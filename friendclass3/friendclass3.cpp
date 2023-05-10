42b5742d1386d2c804a2352dada69c03db0b7826
nadiacahyadewi <nadiacahyadewi20@gmail.com>
2023-05-10 16:20:25 +0700

    memasukan member class pelajar

--- friendclass3/friendclass3.cpp
+++ friendclass3/friendclass3.cpp
@@ -9,3 +9,12 @@ public:
 	void showNilaiPelajar(pelajar&);
 };
 
+class pelajar {
+private:
+	int nilai;
+public:
+	pelajar() {
+		nilai = 100;
+	}
+	friend void manusia::showNilaiPelajar(pelajar& x);
+};
