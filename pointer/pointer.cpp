9bcce1c291aca1076e8078428873347bc4544923
nadiacahyadewi <nadiacahyadewi20@gmail.com>
2023-05-10 15:14:35 +0700

    membuat member access

--- pointer/pointer.cpp
+++ pointer/pointer.cpp
@@ -16,4 +16,8 @@ int main()
 	mahasiswa mhs{ 1 };		//object mhs
 	mhs.showNim();			//member acces operator
 
+	mahasiswa& ref = mhs;	//pointer reference refMhs
+	ref.nim = 2;			//member access operator
+	mhs.showNim();
+
 }
